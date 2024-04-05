b5f07159e71e1ad589dcbc53330b2790c1094b8b
Federico Betti <74817555+federicobetti99@users.noreply.github.com>
2024-04-05 10:57:21 +0200

    Small changes for plots

--- code/lanczos_mc.m
+++ code/lanczos_mc.m
@@ -5,7 +5,7 @@ clc
 
 %% define number of Lanczos iterations, number of samples and averages, width of CI
 ks = [10; 50];
-N = 10000;
+N = 1000;
 avgs = 10;
 alpha = 0.05;
 
@@ -23,14 +23,15 @@ for j = 1:size(ks, 1)
     for l = 1:avgs
         ests = compute_mc_estimator(M, N, W);
         ests = ests + est; % add Lanczos estimate
-        errors = vecnorm(ests-repmat(diaginvM, 1, N)) / norm(diaginvM);
-        avg_errors(j, l, :) = errors;
+        avg_errors(j, l, :) = vecnorm(ests-repmat(diaginvM, 1, N)) / norm(diaginvM);
     end
 end
 
 mean_errors = mean(avg_errors, 2);
 std_dev = std(avg_errors, [], 2) / sqrt(avgs);
 cdi = norminv(1-alpha/2);
+x = (1:N);
+x2 = [x, fliplr(x)];
 
 %% plot and save figure
 fig = figure();
@@ -38,15 +39,12 @@ fig_legend_string = [];
 for j = 1:size(ks, 1)
     curve1 = mean_errors(j, :) + cdi * std_dev(j, :) / sqrt(avgs);
     curve2 = mean_errors(j, :) - cdi * std_dev(j, :) / sqrt(avgs);
-    x = (1:N);
-    x2 = [x, fliplr(x)];
     inBetween = [curve1, fliplr(curve2)];
-    pl = loglog(x, mean_errors(j, :), 'LineWidth', 4);
+    pl = loglog(x, mean_errors(j, :), 'LineWidth', 3);
     hold on
     kstr = sprintf("$k = %.0f$", string(ks(j)));
     fig_legend_string = [fig_legend_string, kstr];
-    c = get(pl,'Color');
-    h = fill(x2, inBetween, c);
+    h = fill(x2, inBetween, get(pl,'Color'));
     set(h, 'facealpha', .2);
     fig_legend_string = [fig_legend_string, ""];
     hold on
